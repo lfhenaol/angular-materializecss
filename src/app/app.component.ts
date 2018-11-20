efa52f98aac06509350981af978f5b119e8d6be8
lfhenaol <lfhenaol@protonmail.com>
2018-11-19 21:23:15 -0500

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import * as M from 'materialize-css';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-materializecss';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('collapsible') elCollapsible: ElementRef;
+  @ViewChild('sidenav') sidenav: ElementRef;
+
+  ngAfterViewInit() {
+    const instanceCollapsible = new M.Collapsible(this.elCollapsible.nativeElement, {});
+    const instanceSideNav = new M.Sidenav(this.sidenav.nativeElement, {});
+  }
 }
